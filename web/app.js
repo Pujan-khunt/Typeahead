420c820703fffcdab00be49983aaa19b4f896acb
Pujan Khunt <pujankhunt2412@gmail.com>
2026-06-18 21:02:26 +0530

    feat(web): implement dynamic suggestion fetching and display

---
+++ web/app.js
@@ -0,0 +1,54 @@
+const suggestion_list = document.getElementById("suggestion-list");
+const search_bar = document.getElementById("search-bar-input");
+
+let currentController = null;
+
+search_bar.addEventListener("input", loadSuggestions);
+search_bar.value = "";
+suggestion_list.replaceChildren();
+
+async function loadSuggestions(input_event) {
+  // Retrieve partial query from user.
+  const prefix = input_event.target.value;
+
+  // Terminate requests early.
+  if (prefix === "" || prefix.length > 50) {
+    suggestion_list.classList.remove("visible");
+    suggestion_list.replaceChildren();
+    return;
+  }
+
+  // Abort previous fetch request.
+  if (currentController) {
+    currentController.abort();
+  }
+  currentController = new AbortController();
+
+  try {
+    // Fetch new suggestions from backend.
+    const suggestions = await fetchSuggestions(prefix, currentController.signal);
+
+    // Remove existing suggestions.
+    suggestion_list.replaceChildren();
+
+    // Display all the suggestions.
+    for (const suggestion of suggestions) {
+      if (suggestion.length < 50) {
+        const list_item = document.createElement('li');
+        list_item.innerHTML = suggestion.substring(0, prefix.length) + "<b>" + suggestion.substring(prefix.length) + "</b>";
+        suggestion_list.appendChild(list_item);
+      }
+    }
+    suggestion_list.classList.add("visible");
+  } catch (error) {
+    if (error.name !== "AbortError") {
+      console.error("Failed to fetch suggestions:", error);
+    }
+  }
+}
+
+async function fetchSuggestions(prefix, signal) {
+  const response = await fetch(`http://127.0.0.1:8080/api/v1/suggestions?prefix="${prefix}"`, { signal });
+  const suggestions = await response.json();
+  return suggestions;
+}
